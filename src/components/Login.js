fcc92ab9057229a940f178d75f1eef40788126ac
Samin1615088 <samin.shafkat@gmail.com>
2021-05-28 05:12:43 +0600

    container, nav, join added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,16 +5,18 @@ const Login = (props) => {
     return (
         <Container>
             <Nav>
-                <a href="/">
-                    <img src="./images/slack.svg" alt="" />
+                <a href="/" >
+                    <img src="./images/slack.svg" alt="app-logo" />
                 </a>
+                <div>
+                    <Join>Join now</Join>
+                </div>
             </Nav>
         </Container>
     );
 };
 
 const Container = styled.div`
-    background: #000;
     padding: 0;
 `;
 
@@ -22,6 +24,7 @@ const Nav = styled.nav`
     max-width: 1128px;
     margin: auto;
     padding: 12px 0  16px;
+    border: 4px;
     display: flex;
     align-items: center;
     position: relative;
@@ -29,22 +32,33 @@ const Nav = styled.nav`
     flex-wrap: nowrap;
 
     & > a {
-        width: 135px;
+        /* width: 135px; */
         height:34px;
         @media (max-width: 768px){
             padding: 0 5px;
         }
     }
-    
+
     /* changed a little bit */
     img{
-        width: 135px;
         height:34px;
-
         @media (max-width: 768px){
             padding: 0 5px;
         }
     }
+    `;
+
+const Join = styled.a`
+        font-size: 16px;
+        padding: 10px 12px;
+        text-decoration: none;
+        color: rgba(0, 0,0, 0.6);
+        margin-right: 12px;
+        &:hover{
+            background-color: rgba(0, 0, 0, 0.08);
+            color: rgba(0, 0, 0, 0.9);
+            text-decoration: none;
+        }
     `;
 
 export default Login;
