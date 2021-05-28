949d482ab6a3180b890bae0af626c8ab576641ec
shaikhjamilalrazi <shaikhjamilalrazi@gmail.com>
2021-05-29 00:17:32 +0600

    tast donee by Jamil

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //import user file
-import './App.css';
-import Login from './components/Login';
-
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                    <Route path="/Home">
+                        <Header />
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
